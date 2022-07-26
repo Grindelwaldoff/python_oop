de9ed20ae479009ac998c8ec8bf69fa33538b2a8
Grindelwaldoff <vsevolodrybnik@gmail.com>
2022-07-26 15:56:13 +0300

    6-е ревью

--- homework.py
+++ homework.py
@@ -45,13 +45,14 @@ class Training():
 
     def get_spent_calories(self):
         """Получить количество затраченных калорий."""
-        raise NotImplementedError(f'В классе {str(self)} не переопределена'
-                                  'функция подсчета каллорий,'
-                                  'данный тип тренировки не поддерживается')
+        raise NotImplementedError(f'В классе {self.__class__.__name__} '
+                                  'не переопределена функция подсчета '
+                                  'каллорий, данный тип тренировки не '
+                                  'поддерживается')
 
     def show_training_info(self) -> InfoMessage:
         """Вернуть информационное сообщение о выполненной тренировке."""
-        return InfoMessage(str(self), self.duration,
+        return InfoMessage(self.__class__.__name__, self.duration,
                            self.get_distance(),
                            self.get_mean_speed(), self.get_spent_calories())
 
@@ -61,9 +62,6 @@ class Running(Training):
     # коэфициенты необходимые для подсчета каллорий потраченных во время бега
     PHYSICAL_ACTIVITY_COEF: int = 18  # коэффициент физической активности
     CALORIE_COEF: int = 20  # еще один коэффициент физической активности
-
-    def __str__(self):
-        return 'Running'
 
     def get_spent_calories(self) -> float:
         return ((self.PHYSICAL_ACTIVITY_COEF * self.get_mean_speed()
@@ -76,9 +74,6 @@ class SportsWalking(Training):
     """Тренировка: спортивная ходьба."""
     ACTIVITY_LEVEL_COEF: float = 0.035  # коэффициент физической активности
     TRAINING_COEF: float = 0.029  # еще один коэффициент физической активности
-
-    def __str__(self):
-        return 'SportsWalking'
 
     def __init__(self,
                  action: int,
@@ -100,9 +95,6 @@ class Swimming(Training):
     PHYSICAL_ACTIVITY_COEF: float = 1.1  # коэффициент физической активности
     CALORIE_COEF: int = 2  # еще один коэффициент физической активности
     LEN_STEP: float = 1.38  # константа: расстояние проходимое за один гребок
-
-    def __str__(self):
-        return 'Swimming'
 
     def __init__(self, action, duration, weight, length_pool: int,
                  count_pool: int, ) -> None:
